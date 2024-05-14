89406cd30cd0dbf88c57563f49ee9ea1827c18e6
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-05-14 22:58:35 +0300

    created mongodb database and connect it to project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 
@@ -10,6 +13,16 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-	console.log('server started on port ==>', PORT);
-});
+const appStart = async () => {
+	try {
+		await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
+		app.listen(PORT, () => {
+			console.log(`Server starter on port: ${PORT}`);
+		});
+	} catch (error) {
+		console.log('error ==>', error);
+	}
+};
+
+appStart();
