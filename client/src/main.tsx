45096063dabe049e711864677befbc8bc498be58
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-05-18 18:57:01 +0300

    created index.css file and import it in main.tsx / forward App.tsx to App folder

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 
-import '@app/styles/normalize.css';
-import '@app/styles/global.css';
+import App from '@app/App.tsx';
+
+import '@app/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
