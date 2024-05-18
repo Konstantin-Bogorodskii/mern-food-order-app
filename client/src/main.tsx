3aea5ba5a0c3aa54cf3debf3aff6bad91207bed4
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-05-18 18:37:54 +0300

    added normalize.css and forward global.css file to app/styles folder

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-import './global.css';
+import '@app/styles/normalize.css';
+import '@app/styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
