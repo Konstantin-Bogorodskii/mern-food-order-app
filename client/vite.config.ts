6ab14a65e95fdf3a894c88ce684c766eae47db70
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-05-18 18:36:50 +0300

    added aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,13 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, './src')
+			'@': path.resolve(__dirname, './src'),
+			'@app': path.resolve(__dirname, './src/app'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@store': path.resolve(__dirname, './src/store')
 		}
+	},
+	server: {
+		port: 3000
 	}
 });
