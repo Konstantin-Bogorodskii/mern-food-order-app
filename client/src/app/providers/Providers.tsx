bb1f59d6f377bb47e440f15e60ac21cb76121621
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-05-19 16:53:59 +0300

    installed react-error-boundary package and used it in Providers component which wrapped all app

---
+++ client/src/app/providers/Providers.tsx
@@ -0,0 +1,10 @@
+import { ErrorBoundary } from 'react-error-boundary';
+import { Fallback } from '@shared/ui/fallback';
+
+interface Props {
+	readonly children: JSX.Element;
+}
+
+export const Providers: React.FC<Props> = ({ children }) => {
+	return <ErrorBoundary FallbackComponent={Fallback}>{children}</ErrorBoundary>;
+};
