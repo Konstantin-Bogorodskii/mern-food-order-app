d79359dfb9d0dab9da2986c655e559918a1f897c
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-05-26 20:45:23 +0300

    created Footer widget

--- client/src/widgets/footer/ui/Footer.tsx
+++ client/src/widgets/footer/ui/Footer.tsx
@@ -1,5 +1,13 @@
 export const Footer = () => {
 	return (
-		<footer className="container mx-auto flex justify-between items-center py-6">Footer</footer>
+		<footer className="bg-orange-500 py-10">
+			<div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+				<span className="text-3xl text-white font-bold tracking-tight">MernEats.com</span>
+				<span className="text-white font-bold tracking-tight flex gap-4">
+					<span>Privacy Policy</span>
+					<span>Terms of Service</span>
+				</span>
+			</div>
+		</footer>
 	);
 };
